7176b4c9982316a7caa595d283ec22b5c96fd0c2
Odbayar Nyamtseren <odbayar.n@gmail.com>
2012-11-14 23:24:18 +0800

    Added a batch file which copies the debug binaries to the system directory.

---
+++ buuz_copy.cmd
@@ -0,0 +1,4 @@
+copy objchk_wxp_x86\i386\buuz.ime C:\Windows\SysWOW64
+copy objchk_wxp_x86\i386\buuz.pdb C:\Windows\SysWOW64
+copy objchk_wnet_amd64\amd64\buuz.ime C:\Windows\System32
+copy objchk_wnet_amd64\amd64\buuz.pdb C:\Windows\System32
