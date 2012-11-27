6c45ff7405f350abfbe0f29dcb783fa07c623750
Odbayar Nyamtseren <odbayar.n@gmail.com>
2012-11-27 15:59:04 +0800

    Bumped the version to 1.0.4

--- Version.h
+++ Version.h
@@ -17,11 +17,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STRING      "1.0.3"
-#define VERSION_NUMBER      "01000300"
+#define VERSION_STRING      "1.0.4"
+#define VERSION_NUMBER      "01000400"
 
 #define VERSION_MAJOR       1
 #define VERSION_MINOR       0
-#define VERSION_SUBMINOR    3
+#define VERSION_SUBMINOR    4
 
 #endif // VERSION_H
